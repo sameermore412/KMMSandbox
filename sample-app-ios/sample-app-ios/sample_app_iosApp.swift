58fb6832134b3f0f72fd39198bfde6d845dee5c3
Sameer More <sameer.more@nytimes.com>
2023-07-17 18:03:09 -0400

    AddIosApp

---
+++ sample-app-ios/sample-app-ios/sample_app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  sample_app_iosApp.swift
+//  sample-app-ios
+//
+//  Created by More, Sameer on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct sample_app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
