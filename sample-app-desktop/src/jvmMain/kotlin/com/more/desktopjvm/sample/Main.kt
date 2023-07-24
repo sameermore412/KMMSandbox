29e1fe549bd59dba2c185170ef1dd5bd95a68edf
Sameer More <sameer.more@nytimes.com>
2023-07-24 12:04:16 -0400

    Update to compose 1.5.0 and kotlin 1.9.0

--- sample-app-desktop/src/jvmMain/kotlin/com/more/desktopjvm/sample/Main.kt
+++ sample-app-desktop/src/jvmMain/kotlin/com/more/desktopjvm/sample/Main.kt
@@ -1,6 +1,11 @@
 package com.more.desktopjvm.sample
 
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.more.common.ui.screens.SimpleScreen
@@ -8,8 +13,10 @@ import com.more.common.ui.screens.SimpleScreen
 fun main() = application {
     Window(
         title = "Sample App",
-        onCloseRequest = ::exitApplication
+        onCloseRequest = ::exitApplication,
     ) {
-       SimpleScreen()
+       Box(modifier = Modifier.fillMaxSize().background(Color.Blue)) {
+           SimpleScreen()
+       }
     }
 }
